58229739f6ad08778c4f3c6a67bc888d9cc42e11
VikramChinnasamy <vikramlatha10@gmail.com>
2023-07-25 11:24:41 +0530

    VAC

---
+++ VAC(DAY 2)/index.js
@@ -0,0 +1,27 @@
+//console.log("Hello World");
+
+// single line comment
+
+/*
+Multi-line
+    comments
+*/
+
+/*
+let name = 'KING OF KINGS';
+console.log(name);
+name = 123;
+console.log(name);
+const movie = "VIKRAM";
+console.log(movie);
+movie = "SALAM";
+*/
+
+let name = "Vikram Chinnasamy";
+let age = "19";
+let completedcollege = false;
+let tech = ["app", "website", "backend"];
+console.log(name);
+console.log(age);
+console.log(completedcollege);
+console.log(tech);
