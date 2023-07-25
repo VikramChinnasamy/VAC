0658d1d15083a96a94ce4f6bff6e89f629d4415f
VikramChinnasamy <vikramlatha10@gmail.com>
2023-07-25 15:30:06 +0530

    file

--- VAC(DAY 2)/index.js
+++ VAC(DAY 2)/index.js
@@ -16,12 +16,60 @@ const movie = "VIKRAM";
 console.log(movie);
 movie = "SALAM";
 */
-
+/*
 let name = "Vikram Chinnasamy";
 let age = "19";
-let completedcollege = false;
-let tech = ["app", "website", "backend"];
-console.log(name);
-console.log(age);
-console.log(completedcollege);
-console.log(tech);
+let amIsingle = true;
+let hobby = ["eating", "sleeping", "riding"];
+let details = {
+    name: "viky",
+    age:19
+};
+let boi = null;
+console.log(typeof(name));
+console.log(typeof(age));
+console.log(typeof(amIsingle));
+console.log(typeof(hobby));
+console.log(typeof(details));
+console.log(typeof(boi));
+console.log(`My name is ${name}. I am ${age} old. My hobbies are ${hobby[0]}, ${hobby[1]}, ${hobby[2]}. I am currently single and that's ${amIsingle}. My nick name is ${details.name}.`);
+
+
+alert('Hii');
+console.log(prompt('Enter a no:'));
+
+*/
+/*
+let a=prompt("Enter a:");
+a=parseInt(a);
+let b=prompt("Enter b");
+b=parseInt(b);
+var c =a+b;
+alert("code executed....");
+alert("The sum is "+ c);
+*/
+
+/*
+const number1 = parseInt(prompt("Enter the first number"));
+const number2 = parseInt(prompt("Enter the second number")); 
+const result = number1 + number2;
+alert(`Addition of 2 number is ${number1} + ${number2}`);
+alert(`Addition of 2 number is ${result}`);
+*/
+
+/*
+let a = prompt("Enter your name");
+alert(`Welcome ${a}`);
+console.log(a);
+*/
+
+const person = {
+    adhar: 3467,
+    name: "Vikram",
+    age: 19,
+    salary: 5
+};
+const person2 = person;
+person.age=25;
+console.log(person);
+console.log(person2);
